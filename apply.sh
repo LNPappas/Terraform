bdf10b252c407e602823b8ac303d860b32a4f5cd
udemy <LPappas@gmail.com>
2020-09-23 13:51:45 -0700

    first commit

---
+++ apply.sh
@@ -0,0 +1,14 @@
+export PATH=$PATH:/opt
+
+if[[ -z "$1"]]; then
+echo ""
+echo "No Terraform path provided"
+echo "SYNTAX = ./apply.sh <PATH>"
+echo "EXAMPLE = ./apply.sh terraform/instance"
+echo ""
+exit
+fi
+
+terraform init $1
+terraform get $1
+terraform apply -auto-approve $1
---
+++ destroy.sh
@@ -0,0 +1,14 @@
+export PATH=$PATH:/opt
+
+if[[ -z "$1"]]; then
+echo ""
+echo "No Terraform path provided"
+echo "SYNTAX = ./destroy.sh <PATH>"
+echo "EXAMPLE = ./destroy.sh terraform/instance"
+echo ""
+exit
+fi
+
+terraform init $1
+terraform get $1
+terraform destroy -auto-approve $1
