6cb07e214e5373123f9403662061332805bd25d7
udemy <LPappas@gmail.com>
2020-09-23 13:33:49 -0700

    buckets

---
+++ buckets/main.tf
@@ -0,0 +1,19 @@
+variable "bucket_name" { default = "bucket-1-udemy-terraform" }
+variable "location" { default = "us-east1" }
+variable "storage_class" { default = "REGIONAL" }
+
+resource "google_storage_bucket" "bucket" {
+    count = "1"
+    name = var.bucket_name
+    location = var.location
+    storage_class = var.storage_class
+
+    labels {
+        name = var.name
+        location = var.location
+    }
+
+    versioning {
+        enabled = true
+    }
+}
