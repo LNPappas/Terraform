f1e81253efeda2a0de69a2e80d566ef69ca5301c
LNPappas <lpappas@gmail.com>
2021-02-05 10:41:18 -0800

    commented out variables

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
-variable "TF_VAR_GOOGLE_CREDENTIALS" {
-  description = "Google service account credentials"
-}
+# variable "TF_VAR_GOOGLE_CREDENTIALS" {
+#   description = "Google service account credentials"
+# }
 
-variable "TF_VAR_PROJECT_ID" {
-  description = "Google project id/name"
-}
+# variable "TF_VAR_PROJECT_ID" {
+#   description = "Google project id/name"
+# }
 
 variable "region" {
   description = "region of project"
