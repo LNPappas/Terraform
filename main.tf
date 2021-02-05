11e20ac1c86babad529577808d6fb4688358900c
LNPappas <lpappas@gmail.com>
2021-02-05 11:06:08 -0800

    removed provider

--- main.tf
+++ main.tf
@@ -1,23 +1,18 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "3.5.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     google = {
+#       source  = "hashicorp/google"
+#       version = "3.5.0"
+#     }
+#   }
+# }
 
-provider "google" {
+# provider "google" {
+#   project = "demotfcloud"
+#   region  = "us-central1"
+#   zone    = "us-central1-c"
+# }
 
-  //credentials = TF_VAR_GOOGLE_CREDENTIALS
-
-  project = "demotfcloud"
-  region  = "us-central1"
-  zone    = "us-central1-c"
-}
-
-resource "google_storage_bucket" "test-bucket" {
-  name          = "terraform-cloud-test-bucket"
-  location      = "us-central1-c"
-  force_destroy = true
+resource "google_storage_bucket" "gcs_bucket" {
+  name = "test-bucket-random-001123"
 }
