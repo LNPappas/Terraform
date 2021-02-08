c186a76db2e0b0cd352ca20fe7a9aaa843e3efdd
LNPappas <lpappas@gmail.com>
2021-02-08 10:08:19 -0800

    added main file

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.55.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "demotfcloud"
+  region  = "us-central1"
+
+}
+
+# resource "google_storage_bucket" "gcs_bucket" {
+#   name = "1st-test-bucket-terraform-cloud-pipeline"
+# }
+
+# resource "google_storage_bucket" "gcs_bucket_2" {
+#   name = "2nd-test-bucket-terraform-cloud-pipeline"
+# }
